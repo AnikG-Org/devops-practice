af483f73240495a1788d17f85f513601f2844eb8
Anik Guha <76238807+Anik-G@users.noreply.github.com>
2022-07-14 15:03:33 +0530

    Update ado_httptrigger.ps1

--- DevOps-common/ado_httptrigger.ps1
+++ DevOps-common/ado_httptrigger.ps1
@@ -4,9 +4,11 @@ Param(
    [string]$user = "",
    [string]$token = "PATtoken",
    [string]$releasedDefinitionId = "1" #release pipeline DefinitionId
+   [int]   $stage_no = 1  #stages Eg= 1,2,3......x
 
 )
 
+$stage = $stage_no-1
 # Base64-encodes the Personal Access Token (PAT) appropriately
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$token)))
 
@@ -15,15 +17,6 @@ function CreateJsonBody
     $value = @"
 {
  "definitionId":$releasedDefinitionId,
-#  "artifacts":[{"alias":"_BitBucket",
-#                 "instanceReference":{"id":"11",
-#                        "name":"11",
-#                        "definitionId":"1",
-#                        "sourceBranch":"master",
-#                     }
-#                 }
-#             ],
-
  "isDraft":false,
  "manualEnvironments":[]
 }
@@ -38,5 +31,27 @@ $url = "$($collectionurl)/$($projectName)/_apis/Release/releases?api-version=6.0
 $result = Invoke-RestMethod -Uri $url -Method Post -Body $json -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
 $ReleaseID = $result.id
+$EnvironmentIds = $result.environments | select id
+$time = $result.createdOn
+$StageEnvironmentId = $result.environments[$($stage)] | select id
+$releaseNo = $result.name
 
+#Output
 Write-Host "ReleaseID:" $ReleaseID
+Write-Host "EnvironmentIds:" $EnvironmentIds
+Write-Host $releaseNo
+##################################### Part 1 [End] used to trigger the release pipeline. if stages for release pipelines are after release trigger then It will deploy all stages.
+
+##################################### Part 2 used to trigger specic stage. need to make sure all stages for release pipelines are 'mannual trigger' only. 
+$envBody='{
+"status": "inProgress"
+}'
+
+#Trigger specific stage / Env
+$EnvUrl = "$($collectionurl)/$($projectName)/_apis/Release/releases/$($ReleaseID)/environments/$($EnvironmentIds[$($stage)].id)?api-version=6.0"
+
+Invoke-RestMethod -Uri $EnvUrl -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo)} -Method patch -Body $envBody -ContentType "application/json"
+
+#Output
+Write-Host $releaseNo $result.status
+Write-Host "Triggered successfully at $time Stage_ReleaseID = $ReleaseID & Stage: $stage_no &  Stahe_$EnvironmentId: $StageEnvironmentId" 
